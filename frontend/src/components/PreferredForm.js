0fd1bfa6e86a6e71c3818b6d573bd0953851b0c6
Pablo <pablo@pom.es>
2020-10-28 11:17:26 +0100

    cleaning

---
+++ frontend/src/components/PreferredForm.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import './../assets/css/various-ui-comp.css'
+import Datepicker from "components/Datepicker.js";
+
+class PreferredForm extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+}
+
+onInputChange(event){
+    this.setState({term: event.target.value});
+}
+
+onFormSubmit(event){
+    event.preventDefault();
+    this.props.onSearch(this.state.term);
+    this.setState({term: ''});
+}
+
+render(){
+    return(     
+      
+          <form method="post" action="/rooms">
+              <div className="row div-form">
+                  <div className="col-md-2">
+                      <div>
+                          <h2>Book Now</h2>
+                          <span>Best Price Online</span>
+                      </div>
+                  </div>
+                  <div className="col-md-3">
+                      <div className="form-group">
+                          <label for="checkin">Check-in:</label>
+                          <div className="form-field">
+                              <i className="icon icon-calendar2"></i>
+                              {/* <input type="text" name="checkin" id="checkin" className="form-control date" placeholder="Check-in date"></input> */}
+                              <Datepicker />
+                          </div>
+                      </div>
+                  </div>
+                  <div className="col-md-3">
+                      <div className="form-group">
+                          <label for="checkout">Check-out:</label>
+                          <div className="form-field">
+                              <i className="icon icon-calendar2"></i>
+                              {/* <input type="text" name="checkout" id="checkout" className="form-control date" placeholder="Check-out date"></input> */}
+                              <Datepicker />
+                          </div>
+                      </div>
+                  </div>
+                  <div className="col-md-2">
+                      <div className="form-group">
+                          <label for="guests">Guest</label>
+                          <div className="form-field">
+                              <i className="icon icon-arrow-down3"></i>
+                              <select name="guests" id="guests" className="form-control">
+                                  <option value="1">1</option>
+                                  <option value="2" selected="">2</option>
+                                  <option value="3">3</option>
+                                  <option value="4">4</option>
+                                  <option value="5">5+</option>
+                              </select>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="col-md-2 btn-cent">
+                      <input className="btn btn-primary btn-block" type="submit" name="submit" id="submit" value="Find Rooms"></input>
+                  </div>
+              </div>
+          </form>
+    )
+  }
+}
+
+
+export default PreferredForm;
--- frontend/src/views/Rooms.js
+++ frontend/src/views/Rooms.js
@@ -3,7 +3,9 @@ import React from "react";
 
 function Rooms(){
 
-
+                return(
+                    <div>Rooms</div>
+                )
 
 
 
